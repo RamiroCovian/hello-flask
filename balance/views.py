4b69bf877e425f664dc7c635df4c73735725d599
RamiroCovian <ramiro.covian@gmail.com>
2023-09-26 17:02:57 -0300

    chore: herencia con multiples bloques

--- balance/views.py
+++ balance/views.py
@@ -20,7 +20,7 @@ def add_movement():
     """
     Crea un movimiento y lo guarda en el archivo CSV
     """
-    return "Agregar nuevo movimiento"
+    return render_template("nuevo.html")
 
 
 @app.route("/modificar")
